da8025162957b38fa431d9f2302849579c429962
Alexandra-Makarenko <s850272@gmail.com>
2023-04-27 15:58:40 +0300

    sort fix
    sort fix

--- src/pages/Home/Home.styled.js
+++ src/pages/Home/Home.styled.js
@@ -1,11 +1,12 @@
 import styled from 'styled-components';
-import bg from '../../images/bg.jpg';
+// import bg from '../../images/bg.jpg';
 
 export const HomeBox = styled.div`
   min-height: 100vh;
   background-color:white;
-  background: url(${bg});
   background-repeat: no-repeat;
   background-size:cover;
   background-attachment: fixed;
 `;
+
+  // background: url(${bg});
--- src/pages/ProductsPage/ProductsFilter/ProductsFilter.jsx
+++ src/pages/ProductsPage/ProductsFilter/ProductsFilter.jsx
@@ -2,7 +2,7 @@ import { useSelector, useDispatch } from "react-redux";
 import { statusFilters } from "redux/products/constants";
 import { getProductsFilter } from "redux/selectors";
 import { setProductsFilter } from "redux/products/filtersSlice";
-import { Button, Stack  } from '@chakra-ui/react'
+import { Button,Menu, MenuButton,MenuList,MenuItemOption , MenuOptionGroup } from '@chakra-ui/react'
 
 export const ProductsFilter = () => {
   const dispatch = useDispatch();
@@ -11,31 +11,51 @@ export const ProductsFilter = () => {
   const handleFilterChange = filter => dispatch(setProductsFilter(filter));
 
   return (
-    <Stack spacing={4} direction='row' align='center' >
-      <Button colorScheme='teal' size='sm'
-        selected={filter === statusFilters.sortByPopularDown}
-        onClick={() => handleFilterChange(statusFilters.sortByPopularDown)}
+      <Menu>
+  <MenuButton as={Button}>
+    Sort by
+  </MenuButton>
+      <MenuList>
+        <MenuOptionGroup defaultValue='sortByPopularDown' title='Order' type='radio'>
+          <MenuItemOption  
+            value='sortByPopularDown'
+            selected={filter === statusFilters.sortByPopularDown}
+            onClick={() => handleFilterChange(statusFilters.sortByPopularDown)}
       >
         PopularDown
-      </Button>
-      <Button colorScheme='teal' size='sm'
-        selected={filter === statusFilters.sortByPopularUp}
-        onClick={() => handleFilterChange(statusFilters.sortByPopularUp)}
+         
+          </MenuItemOption >
+          <MenuItemOption  
+             value='sortByPopularUp'
+             selected={filter === statusFilters.sortByPopularUp}
+             onClick={() => handleFilterChange(statusFilters.sortByPopularUp)}
       >
        PopularUp
-      </Button>
-      <Button colorScheme='teal' size='sm'
-        selected={filter === statusFilters.sortByPriceDown}
-        onClick={() => handleFilterChange(statusFilters.sortByPriceDown)}
+          </MenuItemOption  >
+          <MenuItemOption 
+             value='sortByPriceDown'
+             selected={filter === statusFilters.sortByPriceDown}
+             onClick={() => handleFilterChange(statusFilters.sortByPriceDown)}
       >
         PriceDown
-          </Button>
-          <Button colorScheme='teal' size='sm'
-        selected={filter === statusFilters.sortByPriceUp}
-        onClick={() => handleFilterChange(statusFilters.sortByPriceUp)}
+            </MenuItemOption >
+          <MenuItemOption 
+             value='sortByPriceUp'
+             selected={filter === statusFilters.sortByPriceUp}
+             onClick={() => handleFilterChange(statusFilters.sortByPriceUp)}
       >
        PriceUp
-      </Button>
-    </Stack>
+            </MenuItemOption >
+
+        </MenuOptionGroup>
+     
+           
+            
+  </MenuList>
+      </Menu>
+      
+          
+
+    
   );
 };
