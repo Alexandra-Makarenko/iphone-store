aff317fcbd969b34ebec74e868e284a9f08e3275
Alexandra-Makarenko <s850272@gmail.com>
2023-05-11 17:29:58 +0300

    styles

--- src/pages/ProductsPage/ProductItem/ProductItem.jsx
+++ src/pages/ProductsPage/ProductItem/ProductItem.jsx
@@ -1,6 +1,6 @@
 import { useDispatch } from 'react-redux';
 import { addToCart,removeItem} from '../../../redux/cart/cartSlice';
-import { Button, Box, Image, Badge, Flex,Tooltip } from '@chakra-ui/react'
+import { Button, Box, Image, Badge, Flex,Tooltip,Spacer } from '@chakra-ui/react'
 import { useSelector } from 'react-redux';
 import { Link, useLocation } from "react-router-dom";
 import { BsFillCartCheckFill,BsCartPlus } from 'react-icons/bs'
@@ -18,9 +18,9 @@ export const ProductsItem = ({item}) => {
 
         <Flex p='6' alignItems='baseline' gap={4} wrap='wrap'>
         <Flex alignItems='baseline'>
-          <Badge borderRadius='full' px='2' colorScheme='teal'>
-            {item.state}
-          </Badge>
+          
+            {item.state==='new'? <Badge borderRadius='full' px='2' colorScheme='teal'>{item.state}</Badge>:<Badge borderRadius='full' px='2' colorScheme='yellow'>{item.state}</Badge>}
+          
         </Flex>
 
         <Flex
@@ -36,16 +36,20 @@ export const ProductsItem = ({item}) => {
               </Tooltip>
         </Flex>
 
-        <Box>
-          {item.price}$
-                </Box>
-                              
-                     {cart.find(cartItem => cartItem.id === item.id)?
+          <Flex w='100%'>
+            <Box p='4'>
+              {item.price} $
+            </Box>
+            <Spacer />
+            <Box p='4'>
+              {cart.find(cartItem => cartItem.id === item.id)?
                   <Button colorScheme='teal' size='sm'
                   onClick={() => dispatch(removeItem(item.id))}><BsFillCartCheckFill/></Button> :
                      <Button colorScheme='teal' variant='outline' size='sm'  
-                    onClick={() => dispatch(addToCart(item))}><BsCartPlus/> </Button>  }
-
+                  onClick={() => dispatch(addToCart(item))}><BsCartPlus /> </Button>}
+            </Box>
+                  
+           </Flex>
       </Flex>
     </Box >
   );
